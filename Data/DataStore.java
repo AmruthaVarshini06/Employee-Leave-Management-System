b6ea5b8b80f0eb502892b9a1ea44321f81d41e55
Charishma_aki <charishmaaki@gmail.com>
2026-02-19 18:56:02 +0530

    Create DataStore.java

---
+++ Data/DataStore.java
@@ -0,0 +1,12 @@
+
+package data;
+
+import java.util.ArrayList;
+
+public class DataStore {
+
+    public static ArrayList<Employee> employees = new ArrayList<>();
+    public static ArrayList<Leave> leaves = new ArrayList<>();
+
+}
+
